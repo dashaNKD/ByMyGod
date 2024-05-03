161699cb2f5104d2797456834d663c46fe6f3d5b
Dasha <dashakd10@gmail.com>
2024-05-03 09:07:58 +0000

    Created a login for the counselor- index-user.html Created a stylesheet for it- style.css Updated the script.js file to include the login function

--- ACITY E-Counselling Service/assets/js/script.js
+++ ACITY E-Counselling Service/assets/js/script.js
@@ -31,3 +31,18 @@ $(document).ready(function() {
         }
     });
 });
+
+const signUpButton= document.getElementById('signUpButton');
+const signInButton= document.getElementById('signInButton');
+const signInForm= document.getElementById('signIn');
+const signUpForm= document.getElementById('signUp');
+
+signUpButton.addEventListener('click', () => {
+    signInForm.style.display= 'none';
+    signUpForm.style.display= 'block';
+});
+
+signInButton.addEventListener('click', () => {
+    signInForm.style.display= 'block';
+    signUpForm.style.display= 'none';
+})
